871a021246a09e0142b0ffb2dfa30ad32c714f05
Metroscript <122935820+Metroscript@users.noreply.github.com>
2023-02-03 11:05:10 +0000

    Update roblox.sh

--- roblox.sh
+++ roblox.sh
@@ -5,4 +5,4 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 sudo apt update && sudo apt upgrade -y
-sudo apt isntall -y grapejuice winehq-stable
+sudo apt install -y grapejuice winehq-stable
