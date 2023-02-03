befddbd416b78d09d49c617b05cb0aac1c3a9f8e
Metroscript <122935820+Metroscript@users.noreply.github.com>
2023-02-03 10:15:54 +0000

    Add files via upload

---
+++ roblox.sh
@@ -0,0 +1,8 @@
+sudo dpkg --add-architecture i386
+curl https://gitlab.com/brinkervii/grapejuice/-/raw/master/ci_scripts/signing_keys/public_key.gpg | sudo tee /usr/share/keyrings/grapejuice-archive-keyring.gpg
+sudo tee /etc/apt/sources.list.d/grapejuice.list <<< 'deb [signed-by=/usr/share/keyrings/grapejuice-archive-keyring.gpg] https://brinkervii.gitlab.io/grapejuice/repositories/debian/ universal main'
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y grapejuice winehq-stable
